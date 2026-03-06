959f55551a68cbe655ec2efc9b0924e44d907a0d
bishal10rm <talukdarbishal68@gmail.com>
2026-03-06 15:26:36 +0600

    Added buttons dynamically

--- script.js
+++ script.js
@@ -1,5 +1,22 @@
-fetch("https://openapi.programming-hero.com/api/categories")
-.then((res)=> res.json())
-.then((data) => {
+// fetch("https://openapi.programming-hero.com/api/categories")
+// .then((res)=> res.json())
+// .then((data) => {
+//     console.log(data);
+//  });
+// const categoriesContainer=document.getElementById("categoriesContainer")
+async function loadcategories(){
+    const res=await fetch("https://openapi.programming-hero.com/api/categories");
+    const data=await res.json();
     console.log(data);
- });
+    // console.log(categoriesContainer);
+    data.categories.forEach((category)=>{
+        console.log(category);
+    
+    const btn=document.createElement("button");
+    btn.className="btn btn-outline w-full";
+    btn.textContent=category.category_name;
+    categoriesContainer.append(btn);
+    });
+    // categoriesContainer.innerHTML="I am like js";
+}
+loadcategories();
